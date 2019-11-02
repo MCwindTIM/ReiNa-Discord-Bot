3d848e0a677f52d40ef3b429acf2fc76ed33c38a
MCwindTIM <pw860563576@gmail.com>
2019-11-02 18:25:39 +0800

    新增: eval 指令

---
+++ commands/eval.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const util = require('../util.js');
+const wincmd = require('node-cmd');
+const { inspect } = require('util');
+const botconfig = require("../botconfig.json");
+module.exports.run = async (bot, message, args) =>{
+    message.delete();
+    if(message.author.id === botconfig.OwnerID){
+        let toEval = args.join(" ");
+        let evaluated = inspect(eval(toEval, { depth: 0} ))
+        try {
+
+            if(toEval) {
+                let hrStart = process.hrtime()
+                let hrDiff;
+                hrDiff = process.hrtime(hrStart)
+                const embed = new Discord.RichEmbed()
+                embed
+                .setAuthor(message.author.tag, message.author.avatarURL)
+                .setColor('#0099ff')
+                .setTitle('ReiNa Bot Eval')
+                .setURL("https://mcwind.tk")
+                .setDescription(`*處理時間: ${hrDiff[0] > 0 ? `${hrDiff[0]}s` : ''}${hrDiff[1] / 1000000}ms.*\`\`\`javascript\n${evaluated}\n\`\`\``,{ maxLength:1900 })
+                .setFooter('ReiNa By 一起來當馬猴燒酒吧 (>ω･* )ﾉ#9201', 'https://cdn.discordapp.com/avatars/418095978273570846/17c96d9ce6c135f7511a001e8584db17.png?size=2048');
+                util.sendDeletableMessage(message.channel, { embed }, message.author);
+                return;
+
+            } else {
+                const embed = new Discord.RichEmbed()
+                embed
+                .setAuthor(message.author.tag, message.author.avatarURL)
+                .setColor('#0099ff')
+                .setTitle('ReiNa Bot 錯誤')
+                .setURL("https://mcwind.tk")
+                .setDescription("無法執行eval()函式!")
+                .setFooter('ReiNa By 一起來當馬猴燒酒吧 (>ω･* )ﾉ#9201', 'https://cdn.discordapp.com/avatars/418095978273570846/17c96d9ce6c135f7511a001e8584db17.png?size=2048');
+                util.sendDeletableMessage(message.channel, { embed }, message.author);
+            }
+        } catch(e) {
+            
+        }
+    }
+    else{
+        const embed = new Discord.RichEmbed()
+                embed
+                .setColor('#0099ff')
+                .setTitle('ReiNa Bot 錯誤')
+                .setURL("https://mcwind.tk")
+                .setDescription("權限不足!")
+                .setFooter('ReiNa By 一起來當馬猴燒酒吧 (>ω･* )ﾉ#9201', 'https://cdn.discordapp.com/avatars/418095978273570846/17c96d9ce6c135f7511a001e8584db17.png?size=2048');
+                try {
+                    util.sendDeletableMessage(message.channel, { embed }, message.author);
+                    }   catch (err) {
+                console.error(err);
+                }
+    }
+}
+
+module.exports.help = {
+    name: "eval"
+}
