e50bf0aa85830fda42868445a7f87bceb1c4f5d9
MCwindTIM <pw860563576@gmail.com>
2020-01-27 18:03:47 +0800

    新指令VTC (第一次爬蟲練習)

---
+++ commands/VTC.js
@@ -0,0 +1,51 @@
+const Discord = require("discord.js");
+const util = require('../util.js');
+const superagent = require('superagent');
+const cheerio = require('cheerio');
+module.exports.run = async (bot, message, args) =>{
+	let dataArray = [];
+	message.delete();
+	dataArray = getData("https://myportal.vtc.edu.hk/wps/portal/", dataArray, message);
+}
+
+module.exports.help = {
+	name: "vtc",
+	description: "獲取VTC news 資訊",
+	cate: 8,
+	show: true
+	
+}
+
+function getData(url, dataArray, message){
+    superagent.get(url).set('Accept-Language', 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,zh-CN;q=0.5,und;q=0.4').end((err, res) => {
+        if(err){
+            console.log(`Error: ${err}`)
+        } else {
+			let $ = cheerio.load(res.text);
+			let i;
+			$('p').attr('dir', 'ltr').each((idx, ele) =>{
+				if($(ele).text().match(/[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]/)){
+				dataArray.push($(ele).text());
+				}
+			}) 
+			const embed = new Discord.RichEmbed()
+			embed
+			.setAuthor(message.author.tag, message.author.avatarURL)
+			.setColor('#0099ff')
+			.setTitle('ReiNa Bot')
+			.setURL("https://mcwind.tk")
+			.setDescription(`${message.author}, senpai~ 已經獲取到VTC 最新的消息了`)
+			.setTimestamp()
+			.setFooter('ReiNa By 𝓖𝓻𝓪𝓷𝓭𝓞𝓹𝓮𝓻𝓪𝓽𝓸𝓻#9487', 'https://cdn.discordapp.com/avatars/418095978273570846/17c96d9ce6c135f7511a001e8584db17.png?size=2048');
+			for(let i = 0; i < dataArray.length; i++){
+				embed.addField(`消息 ${i + 1}`, `${dataArray[i]}`);
+			}
+			try {
+				util.sendDeletableMessage(message.channel, { embed }, message.author);
+			}   catch (err) {
+				console.error(err);
+			}
+        }
+	});
+
+}
