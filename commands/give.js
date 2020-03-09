23534d9c7f08422bd15a073fd2369df2dab897bb
MCwindTIM <pw860563576@gmail.com>
2020-03-09 15:42:20 +0800

    database path

--- commands/give.js
+++ commands/give.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const util = require('../util.js');
 const SQLite = require("better-sqlite3");
-const sql = new SQLite('./scores.sqlite');
+const sql = new SQLite('../scores.sqlite');
 module.exports.run = async (bot, message, args) =>{
 	bot.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
 	bot.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
